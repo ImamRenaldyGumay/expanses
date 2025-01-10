f3c377c94db50834c512a769dfb82534eb1ceb81
ImamRenaldy <imam.renaldy26@gmail.com>
2025-01-10 20:56:32 +0700

    fixed

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,17 +5,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         public function __construct() {
             parent::__construct();
-            $this->load->model('Transaction_model');
+            $this->load->model('transaction_model');
         }
 
         public function index() {
             $user_id = $this->session->userdata('user_id');
             
             // Data untuk dashboard
-            $data['total_income'] = $this->Transaction_model->get_total_income($user_id);
-            $data['total_expense'] = $this->Transaction_model->get_total_expense($user_id);
+            $data['total_income'] = $this->transaction_model->get_total_income($user_id);
+            $data['total_expense'] = $this->transaction_model->get_total_expense($user_id);
             $data['current_balance'] = $data['total_income'] - $data['total_expense'];
-            $data['recent_transactions'] = $this->Transaction_model->get_recent_transactions($user_id);
+            $data['recent_transactions'] = $this->transaction_model->get_recent_transactions($user_id);
             // $data['user_id'] = $user_id;
             $data['title'] = 'Dashboard';
 

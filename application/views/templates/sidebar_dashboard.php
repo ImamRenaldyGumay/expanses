4e917a99165d30a70cf1d6900479604ec641c273
ImamRenaldy <imam.renaldy26@gmail.com>
2025-01-10 21:06:28 +0700

    fixed

--- application/views/templates/sidebar_dashboard.php
+++ application/views/templates/sidebar_dashboard.php
@@ -43,7 +43,7 @@
                 </a>
             </li>
             <li>
-                <a href="<?= base_url('add-income')?>" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                <a href="<?= base_url('add-expense')?>" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white">
                         <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z" clip-rule="evenodd" />
                     </svg>
